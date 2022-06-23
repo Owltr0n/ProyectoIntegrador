430b3679403819949b7d13b32647cb0566ba5f59
Owltr0n <94992116+Owltr0n@users.noreply.github.com>
2022-06-23 12:24:56 -0300

    Proyectos

--- Portfolio/src/app/app.module.ts
+++ Portfolio/src/app/app.module.ts
@@ -12,6 +12,7 @@ import { ExperienciaComponent } from './components/experiencia/experiencia.compo
 import { EducacionComponent } from './components/educacion/educacion.component';
 import { NgCircleProgressModule } from 'ng-circle-progress';
 import { HardSoftComponent } from './components/hard-soft/hard-soft.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
 
 @NgModule({
   declarations: [
@@ -23,7 +24,8 @@ import { HardSoftComponent } from './components/hard-soft/hard-soft.component';
     AcercaDeComponent,
     ExperienciaComponent,
     EducacionComponent,
-    HardSoftComponent
+    HardSoftComponent,
+    ProyectosComponent
   ],
   imports: [
     BrowserModule,
